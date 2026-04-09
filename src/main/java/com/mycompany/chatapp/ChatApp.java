01362c3a27507207ee797f945118b4033ee66901
ST10484492 <ST10484492@rcconnect.edu.za>
2026-04-09 13:44:40 +0200

    Initial commit

---
+++ src/main/java/com/mycompany/chatapp/ChatApp.java
@@ -0,0 +1,76 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.chatapp;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author Student
+ */
+public class ChatApp {
+
+    public static void main(String[] args) {
+       Scanner input = new Scanner(System.in);
+        
+        //declaring
+        String userName;
+        String passWord;
+        String phoneNumber;
+        String loginUser;
+        String loginPass;
+        
+        //user input
+        System.out.println("Enter username: ");
+        userName = input.nextLine();
+        
+        System.out.println("Enter password: ");
+        passWord = input.nextLine();
+        
+        System.out.println("Enter your phone number: ");
+        phoneNumber = input.nextLine();
+        
+        //if-else username is valid
+        if(Login.checkUsername(userName)){
+            System.out.println("Username successfully captured.");
+        }else{
+            System.out.println("Username is not correctly formatted; "
+                    + "please ensure that your username containes an underscore "
+                    + "and is no more than five characters in length.");
+        }
+        
+        //if-else password is valid
+        if(Login.checkPassword(passWord)){
+            System.out.println("Password successfully captured.");
+        }else{
+            System.out.println("Password is not correctly formatted; "
+                    + "please ensure that the password contains at least eight character, "
+                    + "a capital letter, a number, and a special character");
+        }
+        
+        //if-else phonenumber is valid
+        if(Login.checkPhoneNumber(phoneNumber)){
+            System.out.println("Cell phone number is successfully captured.");           
+        }else{
+            System.out.println("Cell phone number incorrectly formatted or "
+                    + "does not contain international code");
+        }
+        
+        //Login class method??
+        System.out.println("======LOGIN======");
+        System.out.println("Enter your username: ");
+        loginUser = input.nextLine();
+        
+        System.out.println("Enter your password: ");
+        loginPass = input.nextLine();
+        
+        if(Login.loginUser(userName, passWord, loginUser, loginPass)){
+            System.out.println("Welcome " + userName + ", Great To See You Again");
+        }else{
+            System.out.println("Username is incorect, please try again");
+        }
+    
+    }
+}
---
+++ src/main/java/com/mycompany/chatapp/Login.java
@@ -0,0 +1,39 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.chatapp;
+
+/**
+ *
+ * @author Student
+ */
+class Login {
+     //conditions for username
+    public static boolean checkUsername(String userName){
+        return userName.contains("_")&& userName.length()<=5;
+    }
+    
+    //conditions for password
+    public static boolean checkPassword(String passWord){
+        
+        String pattern = "(?=.*[A-Z])"//atleast one uppercasee
+                       + "(?=.*\\d)" //atleast one number
+                       + "(?=.*[^a-z0-9])"//atleast one special character
+                       + ".{8,}";//atleast 8 characters long
+        
+        return passWord.matches(pattern); //this will return on main class
+    }
+    
+    //conditions for phone number
+    public static boolean checkPhoneNumber(String phoneNumber){
+        String pattern = "^(\\+27|0)[6-8][0-9]{8}";//user should enter +27 and 9number
+        
+        return phoneNumber.matches(pattern);
+    }
+    
+    //has stored all the users input
+    public static boolean loginUser(String storedUser, String storedPass, String loginUser, String loginPass){
+        return loginUser.equals(storedUser) && loginPass.equals(storedPass);
+    }
+}
